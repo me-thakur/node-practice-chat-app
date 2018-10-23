72c0c9fec5afc5f7bb6b1a8c288f0e02e139d6e2
me-thakur <30404593+me-thakur@users.noreply.github.com>
2018-10-23 13:39:23 +0530

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 var socket = io();
 
+function scrollToBottom () {
+  //selectors
+  var messages = $('#messages');
+  var newMessage = messages.children('li:last-child');
+  //height
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function () {
   console.log('connected to the server');
 
@@ -19,6 +35,7 @@ socket.on('newMessage', function (message) {
   });//template want to render
 
   $('#messages').append(html);
+  scrollToBottom();
   // console.log('newMesage', message);
   // var li = $('<li></li>');
   // li.text(`${message.from} ${formattedTime}: ${message.text}`);
@@ -36,6 +53,7 @@ socket.on('newLocationMessage', function (message) {
   });
 
   $("#messages").append(html);
+  scrollToBottom();
   // var li = $('<li></li>');
   // var a = $('<a target="_blank">My current location</a>');
   //
